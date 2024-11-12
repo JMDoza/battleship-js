25c13c80e12bd844ca21e0c61006aaa1245fc4f4
Jimmy <66657082+JMDoza@users.noreply.github.com>
2024-11-11 19:48:07 -0500

    added initialHits parameter and added length, numOfHists, damage, and isSunk methods to Ship object and created associated tests

--- __tests__/dummyTests.js
+++ __tests__/dummyTests.js
@@ -3,12 +3,35 @@ import { Ship } from "../src/battleship";
 describe("Ship Object", () => {
   let ship;
 
-  test("should throw an error if length is not a number", () => {
-    expect(() => Ship("test")).toThrow("Length must be a valid number");
+  beforeEach(() => {
+    ship = Ship(2);
   });
 
-  test("should return correct length", () => {
-    ship = Ship(2);
+  test("should throw an error if length or initialHists is not a number", () => {
+    expect(() => Ship("test")).toThrow("Length must be a valid number");
+    expect(() => Ship(1, "test")).toThrow("initialHits must be a valid number");
+  });
+
+  test("length should return correct value", () => {
     expect(ship.length()).toEqual(2);
+  });
+
+  test("damage should return a new ship object", () => {
+    const damagedShip = ship.damage();
+    expect(damagedShip).toBeDefined();
+    expect(damagedShip).not.toBe(ship);
+  });
+
+  test("numOfHits should return correct number of times hit", () => {
+    const damagedShip = ship.damage().damage();
+    expect(ship.numOfHits()).toEqual(0);
+    expect(damagedShip.numOfHits()).toEqual(2);
+  });
+
+  test("isSunk should return true/false whether ship has sunk", () => {
+    let damagedShip = ship.damage();
+    expect(damagedShip.isSunk()).toBeFalsy();
+    damagedShip = damagedShip.damage();
+    expect(damagedShip.isSunk()).toBeTruthy();
   });
 });
--- src/battleship.js
+++ src/battleship.js
@@ -1,16 +1,22 @@
-function Ship(len) {
+function Ship(len, initialHits = 0) {
   if (typeof len !== "number" || isNaN(len)) {
     throw new Error("Length must be a valid number");
   }
+  if (typeof initialHits !== "number" || isNaN(initialHits)) {
+    throw new Error("initialHits must be a valid number");
+  }
 
-  let hits = 0;
+  const hits = initialHits;
 
-  const length = () => {
-    return len;
-  };
-
+  const length = () => len;
+  const numOfHits = () => hits;
+  const damage = () => Ship(len, hits + 1);
+  const isSunk = () => hits >= len;
   return {
     length,
+    numOfHits,
+    damage,
+    isSunk,
   };
 }
 
